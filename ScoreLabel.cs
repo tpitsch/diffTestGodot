5ccd9ab497b1a6e28c8f260c2ac2f6cf0238b14f
Tyler <tpitsch@gmail.com>
2024-01-11 20:41:01 -0800

    Adding stuff to label

--- ScoreLabel.cs
+++ ScoreLabel.cs
@@ -7,12 +7,14 @@ public partial class ScoreLabel : Label
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		//Editing script to post chaanges to github
 		
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		
 	}
 	private void _on_enemy_health_changed(long val)
 	{
